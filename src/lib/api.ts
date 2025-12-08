ed25aacba18230947808f862c25f0bfb256c8cfb
Ricardo Sanchez <sanchezveraricardo@gmail.com>
2025-12-08 16:19:48 -0500

    Use env() for GoogleScriptMailerrrr

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,14 +3,14 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
-// (Opcional) Solo para debug, luego lo puedes quitar
 console.log("API baseURL en runtime:", baseURL);
 
 export const api = axios.create({
   baseURL,
+  withCredentials: true,   // 👈 ESTO es lo que no está llegando al navegador
 });
 
-// 👇 Si ya hay un token guardado, lo agregamos al header Authorization
+// Si ya hay un token guardado, lo agregamos al header Authorization
 const storedToken = localStorage.getItem("admin_token");
 if (storedToken) {
   api.defaults.headers.common["Authorization"] = `Bearer ${storedToken}`;

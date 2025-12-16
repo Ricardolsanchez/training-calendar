656d3a9ab1fbbefc001030a182e83640168a77a6
Ricardo Sanchez <sanchezveraricardo@gmail.com>
2025-12-16 11:50:06 -0500

    Update backendnewwe

--- src/components/BookingCalendar.tsx
+++ src/components/BookingCalendar.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { api } from "../lib/api";
+import "./BookingCalendar.css";
 
 /** =========================
  *  Types
